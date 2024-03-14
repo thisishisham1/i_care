2f1672d361ea34f4ae3126d45ba8aa350a8fac01
hisham <hishamohmad19@gmail.com>
2024-03-14 21:23:17 +0200

    navigation bar ui done

--- app/src/main/java/com/example/icare/presentation/mainscreen/BottomNavBar.kt
+++ app/src/main/java/com/example/icare/presentation/mainscreen/BottomNavBar.kt
@@ -2,17 +2,32 @@ package com.example.icare.presentation.mainscreen
 
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.icare.R
+import com.example.icare.core.theme.gray400
+import com.example.icare.core.theme.gray500
+import com.example.icare.core.theme.gray600
+import com.example.icare.core.theme.green500
+import com.example.icare.core.theme.green700
+import com.example.icare.core.theme.neutralWhite
 
 private sealed class BottomNavItems(
     val route: String,
@@ -40,7 +55,7 @@ private sealed class BottomNavItems(
     data object Profile : BottomNavItems(
         route = "Profile",
         label = "Profile",
-        vectorIcon = R.drawable.profile2
+        vectorIcon = R.drawable.user
     )
 }
 
@@ -54,25 +69,39 @@ private val _Destinations = arrayOf(
 fun BottomNavBar(navController: NavHostController) {
     val backStackEntry by navController.currentBackStackEntryAsState()
     val currentDestination = backStackEntry?.destination
-    NavigationBar {
-        _Destinations.forEach{
-            AddItem(screen = it, currentDestinations = currentDestination, navController = navController)
+    NavigationBar(containerColor = neutralWhite, tonalElevation = 0.dp) {
+        _Destinations.forEach {
+            AddItem(
+                screen = it,
+                currentDestinations = currentDestination,
+                navController = navController
+            )
         }
     }
 }
 
 @Composable
 private fun RowScope.AddItem(
-    screen:BottomNavItems,
+    screen: BottomNavItems,
     currentDestinations: NavDestination?,
     navController: NavHostController
-){
+) {
     NavigationBarItem(selected =
-        currentDestinations?.hierarchy?.any {
-                                          it.route==screen.route
-        }==true
-        , onClick = { navController.navigate(screen.route)}, icon = { Icon(
-            painter = painterResource(id = screen.vectorIcon),
-            contentDescription =""
-        ) })
+    currentDestinations?.hierarchy?.any {
+        it.route == screen.route
+    } == true,
+        onClick = { navController.navigate(screen.route) },
+        icon = {
+            Icon(
+                painter = painterResource(id = screen.vectorIcon),
+                contentDescription = "", Modifier.size(27.dp)
+            )
+        }, colors = NavigationBarItemDefaults.colors(
+            indicatorColor = neutralWhite,
+            selectedIconColor = green500,
+            selectedTextColor = green700,
+            unselectedIconColor = gray600, unselectedTextColor = gray500
+        ), label = { Text(text = screen.label, style = MaterialTheme.typography.titleSmall) },
+        alwaysShowLabel = false
+    )
 }
